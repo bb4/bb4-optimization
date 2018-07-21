09873dcc59af426a5e5c0d4598178fecff8229c8
barrybecker4 <barrybecker4@yahoo.com>
2018-07-21 05:46:32 -0700

    Scala port. Some cleanup of GlobalHillClimbingStrategy..

--- scala-source/com/barrybecker4/optimization/strategy/GlobalHillClimbingStrategy.scala
+++ scala-source/com/barrybecker4/optimization/strategy/GlobalHillClimbingStrategy.scala
@@ -5,42 +5,43 @@ import com.barrybecker4.optimization.optimizee.Optimizee
 import com.barrybecker4.optimization.parameter.ParameterArray
 
 
-/**
-  * This is a hybrid optimization strategy.
-  *
-  * @see GlobalSampleStrategy
-  * @see HillClimbingStrategy
-  * @author Barry Becker
-  */
 object GlobalHillClimbingStrategy {
   private val NUM_SAMPLES = 1000
 }
 
 /**
-  * Constructor.
-  * use a hardcoded static data interface to initialize.
+  * This is a hybrid optimization strategy.
+  * Use a hardcoded static data interface to initialize.
   * so it can be easily run in an applet without using resources.
-  *
   * @param optimizee the thing to be optimized.
+  * @author Barry Becker
   */
 class GlobalHillClimbingStrategy(optimizee: Optimizee) extends OptimizationStrategy(optimizee) {
 
   /**
     * Perform the optimization of the optimizee.
-    * @param params       parameter array
+    * @param params parameter array
     * @param fitnessRange the approximate absolute value of the fitnessRange.
     * @return optimized params
     */
   override def doOptimization(params: ParameterArray, fitnessRange: Double): ParameterArray = {
+    val sampleResult = doSampleOptimization(params, fitnessRange)
+    doHillClimbingOptimization(sampleResult, fitnessRange)
+  }
+
+  private def doSampleOptimization(params: ParameterArray, fitnessRange: Double): ParameterArray = {
     val gsStrategy = new GlobalSampleStrategy(optimizee)
     gsStrategy.setListener(listener)
     // 3 sample points along each dimension
     gsStrategy.setSamplingRate(GlobalHillClimbingStrategy.NUM_SAMPLES)
     // first find a good place to start
     // perhaps we should try several of the better results from global sampling.
-    val sampledParams = gsStrategy.doOptimization(params, fitnessRange)
+    gsStrategy.doOptimization(params, fitnessRange)
+  }
+
+  private def doHillClimbingOptimization(params: ParameterArray, fitnessRange: Double): ParameterArray = {
     val strategy = new HillClimbingStrategy(optimizee)
     strategy.setListener(listener)
-    strategy.doOptimization(sampledParams, fitnessRange)
+    strategy.doOptimization(params, fitnessRange)
   }
 }
